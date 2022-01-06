604b76c20741958a0ad485d0e23f0257a22a6ea2
Tahiel Giordana <tahielgiordana@gmail.com>
2022-01-06 18:13:57 -0300

    Probando algunas transiciones

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ const resultScreen = document.querySelector(".result");
 
 const options = ["rock", "paper", "scissors", "lizard", "spock"];
 
+let playerChoice;
+let houseChoice;
+
+//Leer el puntaje almacenado
 if (localStorage.getItem("score") == null) {
   scoreText.innerText = 0;
   localStorage.setItem("score", 0);
@@ -26,19 +30,103 @@ if (localStorage.getItem("score") == null) {
   scoreText.innerText = localStorage.getItem("score");
 }
 
-const calcHomeChoice = () => {
-  return options[Math.floor(Math.random() * 5)];
-};
-
 const optionBtns = document.querySelectorAll(".option__btn");
 optionBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    let playerChoice = btn.value;
-    let houseChoice = calcHomeChoice();
-    let result = getResult(playerChoice + houseChoice);
-    showResult(playerChoice, houseChoice, result);
+    playerChoice = btn.value;
+    showResultScreen();
   });
 });
+
+const calcHomeChoice = async () => {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(options[Math.floor(Math.random() * 5)]);
+    }, 1000);
+  });
+
+  let result = await promise;
+  return result;
+};
+
+const showResultScreen = async () => {
+  resetResultScreen();
+  playerChoiceImg.src = "images/icon-" + playerChoice + ".svg";
+  playerChoiceImg.parentElement.classList.add(playerChoice + "-style");
+
+  resultScreen.style.visibility = "visible";
+  resultScreen.style.position = "relative";
+  resultScreen.style.opacity = 1;
+  gameScreen.style.visibility = "hidden";
+  gameScreen.style.position = "absolute";
+  gameScreen.style.opacity = 0;
+
+  houseChoice = await calcHomeChoice();
+  houseChoiceImg.src = "images/icon-" + houseChoice + ".svg";
+  houseChoiceImg.parentElement.classList.add(houseChoice + "-style");
+
+  let result = getResult(playerChoice + houseChoice);
+  showResult(result);
+  setTimeout(() => {
+    showReplayBtn();
+  }, 500);
+};
+
+const showReplayBtn = () => {
+  replayBtn.style.visibility = "visible";
+  replayBtn.style.position = "relative";
+  replayBtn.style.opacity = 1;
+};
+
+const showResult = (result) => {
+  if (result == "You win") {
+    if (parseInt(scoreText.innerText) < 999999) {
+      scoreText.innerText = parseInt(scoreText.innerText) + 1;
+      localStorage.setItem("score", parseInt(scoreText.innerText));
+    }
+    playerChoiceImg.parentElement.classList.add("winner");
+  } else if (result == "You lose") {
+    if (parseInt(scoreText.innerText) > 0) {
+      scoreText.innerText = parseInt(scoreText.innerText) - 1;
+      localStorage.setItem("score", parseInt(scoreText.innerText));
+    }
+    houseChoiceImg.parentElement.classList.add("winner");
+  }
+  resultText.innerText = result;
+  resultText.style.visibility = "visible";
+  resultText.style.position = "relative";
+  resultText.style.opacity = 1;
+};
+
+//Show game screen
+const showGameScreen = () => {
+  gameScreen.style.visibility = "visible";
+  gameScreen.style.position = "relative";
+  gameScreen.style.opacity = 1;
+  resultScreen.style.visibility = "hidden";
+  resultScreen.style.position = "absolute";
+  resultScreen.style.opacity = 0;
+};
+
+const resetResultScreen = () => {
+  playerChoiceImg.parentElement.classList.remove("winner");
+  houseChoiceImg.parentElement.classList.remove("winner");
+  let actualPlayerChoice = playerChoiceImg.parentElement.classList.item(1);
+  playerChoiceImg.parentElement.classList.remove(actualPlayerChoice);
+  let actualHouseChoice = houseChoiceImg.parentElement.classList.item(1);
+  houseChoiceImg.parentElement.classList.remove(actualHouseChoice);
+  playerChoiceImg.src = "";
+  playerChoiceImg.alt = "";
+  houseChoiceImg.src = "";
+  houseChoiceImg.alt = "";
+  resultText.innerText = "";
+  replayBtn.style.visibility = "hidden";
+  replayBtn.style.position = "absolute";
+  replayBtn.style.opacity = 0;
+  resultText.style.visibility = "hidden";
+  resultText.style.position = "absolute";
+  resultText.style.opacity = 0;
+};
 
 const getResult = (choices) => {
   console.log(choices);
@@ -59,10 +147,6 @@ const getResult = (choices) => {
     case "paperspock":
     case "lizardpaper":
     case "spockrock":
-      if (parseInt(scoreText.innerText) < 999999) {
-        scoreText.innerText = parseInt(scoreText.innerText) + 1;
-        localStorage.setItem("score", parseInt(scoreText.innerText));
-      }
       return "You win";
     case "rockpaper":
     case "paperscissors":
@@ -74,69 +158,16 @@ const getResult = (choices) => {
     case "spockpaper":
     case "paperlizard":
     case "rockspock":
-      if (parseInt(scoreText.innerText) > 0) {
-        scoreText.innerText = parseInt(scoreText.innerText) - 1;
-        localStorage.setItem("score", parseInt(scoreText.innerText));
-      }
       return "You lose";
   }
 };
 
-const showResult = (playerChoice, houseChoice, result) => {
-  showResultScreen(true);
-  /*
-  gameScreen.classList.toggle("hidden");
-  resultScreen.classList.toggle("hidden");
-  */
-  //Se seleccionan las imágenes correspondientes
-  playerChoiceImg.src = "images/icon-" + playerChoice + ".svg";
-  let actualPlayerChoice = playerChoiceImg.parentElement.classList.item(1);
-  playerChoiceImg.parentElement.classList.replace(
-    "" + actualPlayerChoice,
-    playerChoice + "-style"
-  );
-
-  houseChoiceImg.src = "images/icon-" + houseChoice + ".svg";
-  let actualHouseChoice = houseChoiceImg.parentElement.classList.item(1);
-  houseChoiceImg.parentElement.classList.replace(
-    "" + actualHouseChoice,
-    houseChoice + "-style"
-  );
-
-  //Se agrega el estilo al ganador
-  playerChoiceImg.parentElement.classList.remove("winner");
-  houseChoiceImg.parentElement.classList.remove("winner");
-  resultText.innerText = result;
-  if (result == "You win") {
-    playerChoiceImg.parentElement.classList.add("winner");
-  } else if (result == "You lose") {
-    houseChoiceImg.parentElement.classList.add("winner");
-  }
-};
-
-//Se oculta el resultado para volver a jugar
 replayBtn.addEventListener("click", () => {
-  showResultScreen(false);
-  /*
-  gameScreen.classList.toggle("hidden");
-  resultScreen.classList.toggle("hidden");
-  */
+  gameScreen.style.visibility = "visible";
+  gameScreen.style.position = "relative";
+  gameScreen.style.opacity = 1;
+  resultScreen.style.visibility = "hidden";
+  resultScreen.style.position = "absolute";
+  resultScreen.style.opacity = 0;
+  resetResultScreen();
 });
-
-const showResultScreen = (bool) => {
-  if (!bool) {
-    gameScreen.style.visibility = "visible";
-    gameScreen.style.position = "relative";
-    gameScreen.style.opacity = 1;
-    resultScreen.style.visibility = "hidden";
-    resultScreen.style.position = "absolute";
-    resultScreen.style.opacity = 0;
-  } else {
-    resultScreen.style.visibility = "visible";
-    resultScreen.style.position = "relative";
-    resultScreen.style.opacity = 1;
-    gameScreen.style.visibility = "hidden";
-    gameScreen.style.position = "absolute";
-    gameScreen.style.opacity = 0;
-  }
-};
